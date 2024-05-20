226ab69ddaaa3e40ddaa036769577929f8c749f9
Logan James Hart <116290186+loganjameshart@users.noreply.github.com>
2024-05-19 19:34:06 -0500

    Add files via upload

---
+++ mousectl.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''
+Runs ratbagctl on startup. Identifies name given to mouse, and sets LED to 0.
+'''
+import subprocess
+
+proc = subprocess.run(['ratbagctl'], stdout=subprocess.PIPE, shell=True)
+
+mouse_name = proc.stdout.decode().split(":")[0]
+
+subprocess.run([f'ratbagctl {mouse_name} led 0 set mode on'], shell=True)
